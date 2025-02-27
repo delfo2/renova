a30644c1a7fdecac5c35c72b121a8440977267d5
delfo <carlos13bem@gmail.com>
2025-02-27 12:46:20 -0300

    feat: message endpoint routes

--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -1,1 +1,80 @@
-export default class MessageController {}
+import { Request, Response } from "express";
+import prisma from "../database/prisma";
+import { MessageService } from "../services/messageService";
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
+import { Speaker } from "@prisma/client";
+import { BadRequestException, Exception } from "../utils/Exception";
+import { ERROR_MESSAGES } from "../utils/Message";
+import { Bot } from "../bot/bot";
+
+export default class MessageController {
+	private messageService = new MessageService(prisma);
+	private bot = new Bot();
+
+	async create(req: Request, res: Response) {
+		try {
+			const { conversationID } = req.params;
+			const { content } = req.body;
+
+			this.checkIfItsUUID(conversationID);
+			this.checkIfItsValidString(content);
+
+			await this.messageService.createMessages(
+				conversationID,
+				content,
+				Speaker.USER
+			);
+			const resBot = await this.bot.process(content);
+			await this.messageService.createMessages(
+				conversationID,
+				resBot,
+				Speaker.BOT
+			);
+			res.status(201).json(resBot);
+		} catch (error) {
+			if (error instanceof Exception) {
+				res.status(error.statusCode).json({ error: error.message });
+			} else {
+				console.log(error);
+				res.status(500).json({ error: "Internal Server Error" });
+			}
+		}
+	}
+
+	async getAll(req: Request, res: Response) {
+		try {
+			const { conversationID } = req.params;
+			this.checkIfItsUUID(conversationID);
+			const messages = await this.messageService.findMessages(
+				conversationID
+			);
+			res.status(200).json(messages);
+		} catch (error) {
+			if (error instanceof Exception) {
+				res.status(error.statusCode).json({ error: error.message });
+			} else {
+				res.status(500).json({ error: "Internal Server Error" });
+			}
+		}
+	}
+
+	private checkIfItsUUID(uuid: string) {
+		if (!uuid || typeof uuid !== "string" || !uuidValidate(uuid)) {
+			throw new BadRequestException(ERROR_MESSAGES.INVALID_UUID);
+		}
+		return true;
+	}
+
+	private checkIfItsValidString(content: string) {
+		if (
+			!content ||
+			typeof content !== "string" ||
+			!/^[\w\s.,!?(){}\[\]$+\-*/=À-ú]+$/.test(content)
+		) {
+			throw new BadRequestException(
+				ERROR_MESSAGES.INVALID_MESSAGE_CONTENT
+			);
+		}
+		return true;
+	}
+}
--- backend/src/services/messageService.ts
+++ backend/src/services/messageService.ts
@@ -1,1 +1,25 @@
-export class MessageService {}
+import { PrismaClient, Speaker } from "@prisma/client";
+
+export class MessageService {
+	constructor(private readonly prismaService: PrismaClient) {}
+
+	async findMessages(conversationID: string) {
+		return await this.prismaService.message.findMany({
+			where: { conversationId: conversationID },
+		});
+	}
+
+	async createMessages(
+		conversationID: string,
+		content: string,
+		speaker: Speaker
+	) {
+		return await this.prismaService.message.create({
+			data: {
+				content,
+				conversationId: conversationID,
+				speaker: speaker,
+			},
+		});
+	}
+}
