e308ed6699bb91020ba24037b004bed2e56e1c65
delfo <carlos13bem@gmail.com>
2025-02-27 12:45:55 -0300

    feat: conversation endpoints routes

--- backend/src/routes/conversationRoutes.ts
+++ backend/src/routes/conversationRoutes.ts
@@ -1,5 +1,14 @@
 import express, { Router } from "express";
+import ConversationController from "../controllers/conversationController";
 
 const conversationRoutes: Router = express.Router();
+const conversationController = new ConversationController();
+
+conversationRoutes.post("/", (req, res) =>
+	conversationController.createConversation(req, res)
+);
+conversationRoutes.get("/", (req, res) =>
+	conversationController.findConversations(req, res)
+);
 
 export default conversationRoutes;
--- backend/src/services/conversationService.ts
+++ backend/src/services/conversationService.ts
@@ -1,1 +1,12 @@
-export class ConversationService {}
+import { PrismaClient } from "@prisma/client";
+
+export class ConversationService {
+	constructor(private readonly prismaService: PrismaClient) {}
+	public async createConversation() {
+		return await this.prismaService.conversation.create({ data: {} });
+	}
+
+	public async findConversations() {
+		return await this.prismaService.conversation.findMany();
+	}
+}
